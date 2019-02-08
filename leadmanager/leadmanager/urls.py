832450b9a282a525beb66dd99f44840e90dd8f5d
Josh Silverman <jrsilverman71@hotmail.com>
2019-02-08 12:57:13 -0800

    django token authentication working on postman

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -1,8 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('frontend.urls')),
-    path('', include('leads.urls'))
+    path('', include('leads.urls')),
+    path('', include('accounts.urls')),
 ]

45ee0989703db11a9268d660723839933ac2087d
Josh Silverman <jrsilverman71@hotmail.com>
2019-02-14 12:47:31 -0800

    frontend authentication login/logout functional

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 import { returnErrors } from "./messages";
-import { USER_LOADED, USER_LOADING, AUTH_ERROR } from "./types";
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS
+} from "./types";
 
 //check the token and load the user
 
@@ -34,5 +41,61 @@ export const loadUser = () => (dispatch, getState) => {
       dispatch({
         type: AUTH_ERROR
       });
+    });
+};
+
+//login user
+
+export const login = (username, password) => dispatch => {
+  //headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  // Request Body
+  const body = JSON.stringify({ username, password });
+
+  axios
+    .post("/api/auth/login", body, config)
+    .then(res => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    });
+};
+
+// logout user
+export const logout = () => (dispatch, getState) => {
+  //get token from state
+  const token = getState().auth.token;
+
+  //headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  //if token, add to headers config
+  if (token) {
+    config.headers["Authorization"] = `Token ${token}`;
+  }
+  axios
+    .post("/api/auth/logout/", null, config)
+    .then(res => {
+      dispatch({
+        type: LOGOUT_SUCCESS
+      });
+    })
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
     });
 };
